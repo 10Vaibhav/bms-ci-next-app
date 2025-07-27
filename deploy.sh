de5b3981d9a261ea437c62ae9b465679893bed4c
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-07-27 21:10:23 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-export PATH=/usr/local/bin:/usr/bin:/usr/sbin:/sbin:$PATH
+export PATH="/home/ubuntu/.nvm/versions/node/v22.17.1/bin:$PATH"
 
-# Use full paths for Node.js tools installed via NVM
-PNPM_PATH="/home/ubuntu/.nvm/versions/node/v22.17.1/bin/pnpm"
-PM2_PATH="/home/ubuntu/.nvm/versions/node/v22.17.1/bin/pm2"
+pnpm install
+pnpm run build
+pm2 restart fe-server
+pm2 restart http-server
+pm2 restart ws-server
 
-git pull
-$PNPM_PATH install
-$PNPM_PATH run build
-$PM2_PATH restart fe-server
-$PM2_PATH restart http-server
-$PM2_PATH restart ws-server
-
-echo "Deployment completed! App is running with PM2."
+echo "Deployment completed!"

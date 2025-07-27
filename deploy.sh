dae85afa43f2243cb23210e2cba88427e903d3a7
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-07-27 20:41:08 +0530

    fixing pipelline

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+
 export PATH=/usr/local/bin:/usr/bin:/usr/sbin:/sbin:$PATH
 
+# Use full paths based on your NVM installation
+PNPM_PATH="/home/ubuntu/.nvm/versions/node/v22.17.1/bin/pnpm"
+PM2_PATH="/home/ubuntu/.nvm/versions/node/v22.17.1/bin/pm2"
 
 git pull
-pnpm install
-pnpm run build
-pm2 restart fe-server
-pm2 restart http-server
-pm2 restart ws-server
+$PNPM_PATH install
+$PNPM_PATH run build
+$PM2_PATH restart fe-server
+$PM2_PATH restart http-server
+$PM2_PATH restart ws-server
 
 echo "Deployment completed! App is running with PM2."

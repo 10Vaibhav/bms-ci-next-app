7fd8f439efa3728eaef11d1188cde6141e0c3ef8
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-07-28 01:02:45 +0530

    change for production only

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ export PATH="/home/ubuntu/.nvm/versions/node/v22.17.1/bin:$PATH"
 pnpm install
 pnpm run build
 pm2 restart fe-server
-pm2 restart http-server
+pm2 restart http_server
 pm2 restart ws-server
 
 echo "Deployment completed!"
